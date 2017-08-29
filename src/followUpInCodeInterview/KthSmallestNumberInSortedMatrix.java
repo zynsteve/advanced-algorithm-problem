f6e761ea18b953d62e25100138e54abe7f8d3ca2
Yinan Zhu <zynsteve@tamu.edu>
2017-08-29 11:43:56 -0500

    kth smallest number in sorted matrix

---
+++ src/followUpInCodeInterview/KthSmallestNumberInSortedMatrix.java
@@ -0,0 +1,64 @@
+package followUpInCodeInterview;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Find the kth smallest number in at row and column sorted matrix.
+ * 
+ * Example
+ * Given k = 4 and a matrix:
+ * [
+ *   [1 ,5 ,7],
+ *   [3 ,7 ,8],
+ *   [4 ,8 ,9],
+ * ]
+ * return 5
+ */
+
+class Pair {
+    public int x, y, val;
+    public Pair(int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+}
+
+class PairComparator implements Comparator<Pair> {
+    public int compare(Pair a, Pair b) {
+        return a.val - b.val;
+    }
+}
+
+public class KthSmallestNumberInSortedMatrix {
+    /**
+     * @param matrix: a matrix of integers
+     * @param k: an integer
+     * @return: the kth smallest number in the matrix
+     */
+    public int kthSmallest(int[][] matrix, int k) {
+        int[] dx = new int[]{0, 1};
+        int[] dy = new int[]{1, 0};
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[][] hash = new boolean[n][m];
+        PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>(k, new PairComparator());
+        minHeap.add(new Pair(0, 0, matrix[0][0]));
+
+        for (int i = 0; i < k - 1; i ++) {
+            Pair cur = minHeap.poll();
+            for (int j = 0; j < 2; j ++) {
+                int next_x = cur.x + dx[j];
+                int next_y = cur.y + dy[j];
+                Pair next_Pair = new Pair(next_x, next_y, 0);
+                if (next_x < n && next_y < m && !hash[next_x][next_y]) {
+                    hash[next_x][next_y] = true;
+                    next_Pair.val = matrix[next_x][next_y];
+                    minHeap.add(next_Pair);
+                }
+            }
+        }
+        return minHeap.peek().val;
+    }
+}
